a72b7a084ec3c0c563f09b269860b0260a750ecf
clarajeon <clarajeon126@gmail.com>
2021-06-18 19:19:48 -0400

    view controllers created

--- OnlyCats/View Controllers/CatViewController.swift
+++ OnlyCats/View Controllers/CatViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class CatViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
---
+++ OnlyCats/View Controllers/PawsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PawsViewController.swift
+//  OnlyCats
+//
+//  Created by Clara Jeon on 6/18/21.
+//
+
+import UIKit
+
+class PawsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
